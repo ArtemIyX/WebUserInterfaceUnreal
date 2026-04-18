b5e913f89d214eb904513bae84d2b7e1f898e581
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-04-18 12:11:00 +0300

    Web socket server

---
+++ Source/CefWebSocketServer/Private/CefWebSocketServer.cpp
@@ -0,0 +1,17 @@
+﻿#include "CefWebSocketServer.h"
+
+#define LOCTEXT_NAMESPACE "FCefWebSocketServerModule"
+
+void FCefWebSocketServerModule::StartupModule()
+{
+    
+}
+
+void FCefWebSocketServerModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCefWebSocketServerModule, CefWebSocketServer)
---
+++ Source/CefWebSocketServer/Public/CefWebSocketServer.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FCefWebSocketServerModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+};
