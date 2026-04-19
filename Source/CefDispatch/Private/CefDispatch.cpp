471281a0c53ab02e6bd2182b1e4c5f108962dfa8
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-04-19 18:22:42 +0300

    Cef dispatch module

---
+++ Source/CefDispatch/Private/CefDispatch.cpp
@@ -0,0 +1,17 @@
+﻿#include "CefDispatch.h"
+
+#define LOCTEXT_NAMESPACE "FCefDispatchModule"
+
+void FCefDispatchModule::StartupModule()
+{
+    
+}
+
+void FCefDispatchModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCefDispatchModule, CefDispatch)
