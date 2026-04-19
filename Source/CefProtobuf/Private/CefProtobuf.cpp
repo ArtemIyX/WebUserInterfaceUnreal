05e0f548b204394e08a89612e15bfc4e2fec4517
Artem Podorozhko <artem.podorozhko@outlook.com>
2026-04-19 10:49:39 +0300

    Protobuf module

---
+++ Source/CefProtobuf/Private/CefProtobuf.cpp
@@ -0,0 +1,17 @@
+﻿#include "CefProtobuf.h"
+
+#define LOCTEXT_NAMESPACE "FCefProtobufModule"
+
+void FCefProtobufModule::StartupModule()
+{
+    
+}
+
+void FCefProtobufModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FCefProtobufModule, CefProtobuf)
